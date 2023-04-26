36986c0c027525496745dc640479e68b04ea445a
Chaitanya Prakash <93187204+Vivylius@users.noreply.github.com>
2023-04-26 10:13:45 +0530

    Delete src directory

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Landing } from "./Landing";
-export { default as Error } from "./Error";
-export { default as Register } from './Register';
-export { default as ProtectedRoute } from "./ProtectedRoute";
